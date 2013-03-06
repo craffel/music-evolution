243c0c40e3efc1fe91bbdecb3722687aaed23952
Colin Raffel <craffel@gmail.com>
2013-03-05 20:43:12 -0500

    Removing extraneous files from the msd module - I may add these back if I find out I need them.  They were causing annoying import problems.

--- msd/__init__.py
+++ msd/__init__.py
@@ -1,1 +1,1 @@
-import create_aggregate_file, create_summary_file, display_song, enpyapi_to_hdf5, hdf5_descriptors, hdf5_getters, hdf5_to_matfile, hdf5_utils, beat_aligned_feats
+import hdf5_getters, beat_aligned_feats
