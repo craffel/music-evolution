0e6e899cd6ca5e5092cdcecd3059858d5464c09b
Colin Raffel <craffel@gmail.com>
2013-03-10 17:33:05 -0400

    Added code to set paths as a config and also test that everything is installed and set up correctly before running (so someone else can run this code and get sensible errors).  Also changed createDataset so that it creates 10 random subsamplings instead of 1, and moved stuff around so that things are only created when they don't exist and only when the script is run (as opposed to imported).

---
+++ paths.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Various global paths needed for the experiment - set these before trying to run anything!
+
+# <codecell>
+
+# The path to the MSD
+msdPath = "/Volumes/MILLIONSONG/data"
+# The path where the random subsampling of the MSD will be/is saved, including the timbre sample.  Should be able to store 60+ GB of data
+subsamplePath = "./Data/"
+# The path (with filename) of the .csv filelist of all MSD files will be/is saved.  Should end in .csv
+fileListName = "./Data/MSDFileList.csv"
+# The path (with filename) of the .txt file included with the MSD which lists the items in the MSD by year.  Should end in .txt
+yearToFileMappingName = "/Volumes/MILLIONSONG/tracks_per_year.txt"
+
---
+++ test.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# This should test that all of the paths are set correctly and necessary modules are installed.
+
+# <codecell>
+
+# Test for modules needed
+try:
+    import os
+    import csv
+    import collections
+    import random
+    import sys
+except:
+    print "Failed to import base Python modules - is there something wrong with your Python install?  Are you using Python 2.7?"
+    sys.exit(1)
+try:
+    import networkx as nx
+except:
+    print "networkx not installed.  Please install from http://networkx.github.com/"
+    sys.exit(1)
+try:
+    import numpy as np
+except:
+    print "numpy not installed.  Please install from http://www.numpy.org/"
+    sys.exit(1)
+try:
+    import matplotlib.pyplot as plt
+except:
+    print "matplotlib not installed.  Please install from http://matplotlib.org/"
+    sys.exit(1)
+try:
+    import msd
+except:
+    print "msd not found - this should have been part of the package you downloaded.  Did you mess with the directory structure?"
+    sys.exit(1)
+try:
+    import tables
+except:
+    print "pytables not installed.  Please install from http://www.pytables.org/moin"
+    sys.exit(1)
+try:
+    import scipy.stats.mstats
+except:
+    print "scipy(.stats) not found.  Please install from http://scipy.org/"
+    sys.exit(1)
+try:
+    import paths
+except:
+    print "Couldn't find paths.py, which should have been part of the package you downloaded.  Did you mess with the directory structure?"
+
+# <codecell>
+
+# Test that paths.py was setup correctly
+if not os.path.exists( paths.msdPath ):
+    print "The msdPath variable you specified in paths.py doesn't exist.  Please create it or edit paths.py and try again."
+    sys.exit(1)
+if not os.path.exists( paths.subsamplePath ):
+    print "The subsamplePath variable you specified in paths.py doesn't exist.  Please create it or edit paths.py and try again."
+    sys.exit(1)
+if not os.path.exists( os.path.split( paths.fileListName )[0] ):
+    print "The directory of the fileListName you specified in paths.py doesn't exist, so the file will not be able to be created.  Please create it or edit paths.py and try again."
+    sys.exit(1)
+if not os.path.exists( os.path.split( paths.yearToFileMapping )[0] ):
+    print "The directory of the yearToFileMapping you specified in paths.py doesn't exist, so the file will not be able to be created.  Please create it or edit paths.py and try again."
+    sys.exit(1)
+
